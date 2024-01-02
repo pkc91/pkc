7eb01f3c4465f3ce8836fdead79651d48544786d
pkc <chavanparesh91@gmail.com>
2024-01-02 16:34:05 +0530

    first commit

---
+++ contiq/src/test/java/com/pkc/contiq/ContiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkc.contiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContiqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
